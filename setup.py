5808ea04e31c9ce8446023bd3a435e2d91c0a419
mrjslau <marijuslau@yahoo.com>
2018-05-14 10:52:13 +0300

    Noddy2 (not working)

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup, Extension
 MODN = "noddy"
 MODS = "spam"
+MODND = "noddy2"
 
 moduleNoddy = Extension(MODN, sources = ["noddymodule.c"])
 setup(name = MODN, ext_modules = [moduleNoddy])
 
 moduleSpam = Extension(MODS, sources = ["spammodule.c"])
 setup(name = MODS, ext_modules = [moduleSpam])
+
+moduleNoddy = Extension(MODND, sources = ["noddy2module.c"])
+setup(name = MODND, ext_modules = [moduleNoddy])
